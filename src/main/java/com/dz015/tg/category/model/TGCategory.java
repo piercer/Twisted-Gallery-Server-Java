0fd127fc58f2018889b0d2eb36b90af01f43debc
Conrad Winchester <conrad@chiwestern.com>
2013-06-25 22:17:20 +0100

    CategoryDetails call is working.

--- src/main/java/com/dz015/tg/category/model/TGCategory.java
+++ src/main/java/com/dz015/tg/category/model/TGCategory.java
@@ -13,4 +13,8 @@ public class TGCategory {
     public String getName() {
         return name;
     }
+
+    public void addParent(long parentID, String parentName) {
+        System.out.println("Adding parent: "+parentID+">"+parentName);
+    }
 }
--- src/main/java/com/dz015/tg/service/data/MySqlTGDataService.java
+++ src/main/java/com/dz015/tg/service/data/MySqlTGDataService.java
@@ -5,7 +5,9 @@ import com.dz015.tg.collection.model.TGCollection;
 
 import javax.inject.Inject;
 import javax.sql.DataSource;
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MySqlTGDataService implements TGDataService {
@@ -16,10 +18,15 @@ public class MySqlTGDataService implements TGDataService {
     @Override
     public TGCollection getCollection(long collectionId) {
 
-        try (Connection con = dataSource.getConnection()) {
-
+        try (
+                Connection con = dataSource.getConnection();
+                CallableStatement statement = con.prepareCall("call getCollectionItems(?)")
+        ) {
+            statement.setLong(1, collectionId);
+            ResultSet rs = statement.executeQuery();
         }
         catch (SQLException e) {
+            System.out.println(e.toString());
             //JDBCTutorialUtilities.printSQLException(e);
         }
         return new TGCollection(collectionId);
@@ -27,7 +34,38 @@ public class MySqlTGDataService implements TGDataService {
 
     @Override
     public TGCategory getCategory(long categoryId) {
-        return null;
+        TGCategory category = new TGCategory(categoryId);
+        try (
+                Connection con = dataSource.getConnection();
+                CallableStatement statement = con.prepareCall("call getCategoryDetails(?)")
+        ) {
+            statement.setLong(1, categoryId);
+            statement.execute();
+            //
+            // Category parents are in the first rewsult set
+            //
+            ResultSet rs = statement.getResultSet();
+            while (rs.next()) {
+                long parentID = rs.getLong(1);
+                String parentName = rs.getString(2);
+                category.addParent(parentID, parentName);
+            }
+            //
+            // Child categories are next
+            //
+            statement.getMoreResults();
+            rs = statement.getResultSet();
+            //
+            // Then child collections
+            //
+            statement.getMoreResults();
+            rs = statement.getResultSet();
+        }
+        catch (SQLException e) {
+            System.out.println(e.toString());
+            //JDBCTutorialUtilities.printSQLException(e);
+        }
+        return category;
     }
 
 }
