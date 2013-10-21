fed1dcd41e3399403f913d8a044b7fd48010edbf
Conrad Winchester <conrad@chiwestern.com>
2013-10-21 13:31:28 +0100

    Added image resizing - it does not maintain aspect ration yet

--- src/main/java/com/dz015/tg/item/resource/ItemResource.java
+++ src/main/java/com/dz015/tg/item/resource/ItemResource.java
@@ -2,6 +2,7 @@ package com.dz015.tg.item.resource;
 
 import javax.imageio.ImageIO;
 import javax.ws.rs.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
@@ -11,31 +12,23 @@ import java.net.URL;
 public class ItemResource {
 
     @GET
-    public BufferedImage getFullImage(@PathParam("id") int id, @PathParam("width") int width, @PathParam("height") int height) {
+    public BufferedImage getImage(@PathParam("id") int id, @PathParam("width") int width, @PathParam("height") int height) {
 
-        BufferedImage image;
+        final BufferedImage original;
+        final BufferedImage resized = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+        final Graphics2D graphics2D = resized.createGraphics();
 
         try {
-            image = ImageIO.read(new URL("file:///Users/conrad/Documents/fitear2/DSC04525.jpg"));
+            original = ImageIO.read(new URL("file:///Users/conrad/Documents/WeddingPhotos/DSC03626.jpg"));
+            graphics2D.addRenderingHints(new RenderingHints(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY));
+            graphics2D.drawImage(original,0,0,width,height,null);
+            graphics2D.dispose();
         }
         catch (IOException e) {
             throw new WebApplicationException(404);
         }
 
-        //ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-        //try {
-        //    ImageIO.write(image, "png", baos);
-        //} catch (IOException e) {
-        //    throw new WebApplicationException(404);
-        // }
-        //byte[] imageData = baos.toByteArray();
-
-        // uncomment line below to send non-streamed
-        // return Response.ok(imageData).build();
-
-        // uncomment line below to send streamed
-        return image;
+        return resized;
     }
 
 }
